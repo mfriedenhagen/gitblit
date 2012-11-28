9f41ce4d1c754689c08951c6bf789c56e0852b45
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2012-11-28 15:30:46 +0100

    Set version number to indicate internal

--- src/com/gitblit/Constants.java
+++ src/com/gitblit/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.0-unitedinternet-1";
 
 	// The build script extracts this exact line so be careful editing it
 	// and only use A-Z a-z 0-9 .-_ in the string.
